249fb916a4733f05f7c72d30702fe395c9588e6c
knights2126 <36615551+knights2126@users.noreply.github.com>
2018-02-20 07:22:14 -0500

    Add files via upload

---
+++ P2T1_SalesPrediction_KnightSteven.py
@@ -0,0 +1,11 @@
+#CTI_110
+#P2T1_SalesPrediction
+#Steven_Knight
+#2/18/18
+
+Total_sales = float(input("Enter the projected sales  "))
+Profit = Total_sales * 0.23
+print("The profit is $",Profit)
+
+
+      
